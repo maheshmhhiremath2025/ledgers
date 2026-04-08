71111bc384c837509b0fa41f9a4bb3c5ad0898aa
maheshmhhiremath2025 <maheshmhhiremath@gmail.com>
2026-04-08 11:25:46 +0530

    favicon: rename double-extension file, globally wire in _document

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" data-theme="light">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+        <meta name="theme-color" content="#6366F1" />
+      </Head>
       <body>
         <Main />
         <NextScript />
